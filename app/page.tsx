394d6c56cdd18fd510cd22bd0d486a51e5555e81
piyawatpm <piyawat_00101@hotmail.com>
2024-10-08 09:44:47 +1100

    fix: fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,10 @@ import Header from "./_components/Header";
 import Hero from "./_components/Hero";
 
 export default function Home() {
-  const [isAnimating, setIsAnimating] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsAnimating(true);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="min-h-screen bg-black">
-      <Header isAnimating={isAnimating} />
-      <Hero isAnimating={isAnimating} />
+      <Header />
+      <Hero />
     </div>
   );
 }
